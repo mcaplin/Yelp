3ad499ffadf09727e832d539fdf399ad4476cd9d
Michelle Caplin <michellecaplin@capcap.local>
2018-02-19 23:19:34 -0800

    complete

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,25 +8,63 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UIScrollViewDelegate {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var searchController: UISearchController!
+    
+    var isMoreDataLoading = false
+    
     
     var businesses: [Business]!
+    //var filteredBusinesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+        
+        // Initializing with searchResultsController set to nil means that
+        // searchController will use this view controller to display the search results
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        
+        // If we are using this same view controller to present the results
+        // dimming it out wouldn't make sense. Should probably only set
+        // this to yes if using another controller to display the search results.
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.searchBar.sizeToFit()
+        tableView.tableHeaderView = searchController.searchBar
+        
+        // Sets this view controller as presenting view controller for the search interface
+        definesPresentationContext = true
+        
+        
+        
+        
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
-            if let businesses = businesses {
+            self.tableView.reloadData()
+            /*if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
                     print(business.address!)
                 }
-            }
+            }*/
             
             }
         )
+        //filteredBusinesses = businesses
+
         
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
@@ -41,10 +79,45 @@ class BusinessesViewController: UIViewController {
         
     }
     
+    func updateSearchResults(for searchController: UISearchController) {
+      
+        if let searchText = searchController.searchBar.text {
+            Business.searchWithTerm(term: "Thai " + searchText, completion: { (businesses: [Business]?, error: Error?) -> Void in
+                
+                self.businesses = businesses
+                self.tableView.reloadData()
+                /*if let businesses = businesses {
+                 for business in businesses {
+                 print(business.name!)
+                 print(business.address!)
+                 }
+                 }*/
+                
+            }
+            )
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        }
+        else {
+            return 0
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        return cell
+    }
+    
     
     /*
      // MARK: - Navigation
